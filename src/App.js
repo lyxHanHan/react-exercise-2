4067b83bd635256d79e360e18de3b86e1078d64f
liyixuan <13069579956@163.com>
2020-09-24 00:30:29 +0800

    add item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import './App.scss';
+import Head from './components/Head';
+import Item from './components/Item';
 
 class App extends Component {
   render() {
     return (
       <main className="app">
+        <Head />
+        <Item />
       </main>
     );
   }
---
+++ src/components/Item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import '../styles/item.scss';
+
+class Item extends React.Component {
+  render() {
+    return <div className="item"></div>;
+  }
+}
+export default Item;
