9bbaffc42671d95ccc9cf5c8478e49515a4fc779
liyixuan <13069579956@163.com>
2020-09-24 00:42:38 +0800

    huoqu status

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,6 +2,20 @@ import React from 'react';
 import '../styles/item.scss';
 
 class Item extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [
+        { categroy: '', name: '', price: '' },
+        { categroy: '', name: '', price: '' },
+        { categroy: '', name: '', price: '' },
+        { categroy: '', name: '', price: '' },
+        { categroy: '', name: '', price: '' },
+        { categroy: '', name: '', price: '' },
+      ],
+    };
+  }
+
   render() {
     return <div className="item"></div>;
   }
