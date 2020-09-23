6d845764d2e2208a40a4880e00bd804af5aaf7ba
liyixuan <13069579956@163.com>
2020-09-24 00:35:34 +0800

    add pic

--- src/components/Head.js
+++ src/components/Head.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import '../styles/head.scss';
+import img from '../assets/shoppoingCar.jpg';
 
 class Head extends React.Component {
   render() {
     return (
       <div>
         <h1 className="store">store</h1>
+        <img src={img}></img>
       </div>
     );
   }
