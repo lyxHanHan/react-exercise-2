3d12fa7838a4dc70ef93b8bd0365f659cff0c8ca
liyixuan <13069579956@163.com>
2020-09-24 00:21:01 +0800

    add head

---
+++ src/components/Head.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../styles/head.scss';
+
+class Head extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 className="store">store</h1>
+      </div>
+    );
+  }
+}
+export default Head;
