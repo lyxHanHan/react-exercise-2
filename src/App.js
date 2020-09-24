944093573a0cbbd687c649fe6ff01e86e6362566
liyixuan <13069579956@163.com>
2020-09-24 17:11:46 +0800

    add buy

--- src/App.js
+++ src/App.js
@@ -4,11 +4,22 @@ import Head from './components/Head';
 import Item from './components/Item';
 
 class App extends Component {
+  state = {
+    count: 0,
+  };
+
+  buy = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+    console.log(this.state.count);
+  };
+
   render() {
     return (
       <main className="app">
-        <Head />
-        <Item />
+        <Head count={this.state.count} />
+        <Item buy={this.state.buy} />
       </main>
     );
   }
